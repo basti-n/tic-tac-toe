863a6ed7fd803fd1d4bd8323e4277633c43ae483
Sebastian Neumair <sebastianneumair@Sebastians-MBP.fritz.box>
2020-10-03 21:32:45 +0200

    Add Empty Gametable for /result endpoint [POST]

--- api/controllers/resultController.js
+++ api/controllers/resultController.js
@@ -1,3 +1,16 @@
+const gametable = require('../constants/game-table');
+
 module.exports.post_result = (req, res) => {
-  res.json({ result: true });
+  res.json(createEmptyGameResponse());
 };
+
+function createGametable() {
+  return gametable;
+}
+
+function createEmptyGameResponse() {
+  return {
+    game: createGametable(),
+    completed: false
+  }
+}
