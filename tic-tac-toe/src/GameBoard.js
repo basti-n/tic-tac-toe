feecd5f09e89069c81b93765360cfe36a6e9735d
Miriam <miriam.haenle@gmail.com>
2020-10-04 17:30:20 +0200

    Add playMove function

--- tic-tac-toe/src/GameBoard.js
+++ tic-tac-toe/src/GameBoard.js
@@ -1,29 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function GameBoard() {
   const initialState = {
     game: [
-      { value: 'x', id: 'ttt_1' },
+      { value: null, id: 'ttt_1' },
       { value: null, id: 'ttt_2' },
-      { value: 'x', id: 'ttt_3' },
+      { value: null, id: 'ttt_3' },
       { value: null, id: 'ttt_4' },
       { value: null, id: 'ttt_5' },
-      { value: 'o', id: 'ttt_6' },
+      { value: null, id: 'ttt_6' },
       { value: null, id: 'ttt_7' },
       { value: null, id: 'ttt_8' },
-      { value: 'x', id: 'ttt_9' },
+      { value: null, id: 'ttt_9' },
     ],
     completed: false,
     winner: null,
   }
-  const [gameStatus, setGameStatus] = useState(initialState)
 
+  /*   useEffect(() => {
+    const updatedGameStatus = axios
+      .post('http://localhost:3000/result')
+      .then((result) => console.log(result))
+  }, []) */
+
+  const [gameStatus, setGameStatus] = useState(initialState)
   const currentGame = gameStatus.game
+
+  function playMove(event) {
+    const cellToUpdate = gameStatus.game.findIndex(
+      (cell) => cell.id === event.target.id
+    )
+    const newGameCells = [...gameStatus.game]
+    newGameCells[cellToUpdate] = { value: 'X', id: event.target.id }
+    const updatedGame = newGameCells
+    setGameStatus({ ...gameStatus, game: updatedGame })
+  }
 
   return (
     <div className="board__grid">
       {currentGame.map((gameValue) => (
-        <div key={gameValue.id} className="board__item">
+        <div
+          key={gameValue.id}
+          className="board__item"
+          id={gameValue.id}
+          onClick={playMove}
+        >
           {gameValue.value}
         </div>
       ))}
