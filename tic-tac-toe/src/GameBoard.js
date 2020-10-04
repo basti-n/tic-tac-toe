c9c01c9810454f9580beeaac425151690e5d1df4
Miriam <miriam.haenle@gmail.com>
2020-10-04 15:01:00 +0200

    Add axios

--- tic-tac-toe/src/GameBoard.js
+++ tic-tac-toe/src/GameBoard.js
@@ -1,21 +1,23 @@
 import React, { useState } from 'react'
 
 export default function GameBoard() {
-  const [gameStatus, setGameStatus] = useState({
+  const initialState = {
     game: [
-      { value: null, id: 'top-left' },
-      { value: null, id: 'top-middle' },
-      { value: 'x', id: 'top-rigth' },
-      { value: null, id: 'middle-left' },
-      { value: null, id: 'middle-middle' },
-      { value: 'o', id: 'middle-rigth' },
-      { value: null, id: 'bottom-left' },
-      { value: null, id: 'bottom-middle' },
-      { value: 'x', id: 'bottom-rigth' },
+      { value: 'x', id: 'ttt_1' },
+      { value: null, id: 'ttt_2' },
+      { value: 'x', id: 'ttt_3' },
+      { value: null, id: 'ttt_4' },
+      { value: null, id: 'ttt_5' },
+      { value: 'o', id: 'ttt_6' },
+      { value: null, id: 'ttt_7' },
+      { value: null, id: 'ttt_8' },
+      { value: 'x', id: 'ttt_9' },
     ],
     completed: false,
     winner: null,
-  })
+  }
+  const [gameStatus, setGameStatus] = useState(initialState)
+
   const currentGame = gameStatus.game
 
   return (
