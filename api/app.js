471d704e3ac81be85a8e18fc9b599e78ee284957
Sebastian Neumair <sebastianneumair@Sebastians-MBP.fritz.box>
2020-10-04 16:55:19 +0200

    Add CORS

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const resultRoutes = require('./routes/result');
 
 const app = express();
 const port = process.env.port || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use('/result', resultRoutes);
 
 app.listen(port, () => console.log(`App listening on port ${port}.`));
