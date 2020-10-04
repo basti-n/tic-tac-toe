fb9da0b68751d4c5dcd00803a5d4df1410c50e09
Sebastian Neumair <sebastianneumair@Sebastians-MBP.fritz.box>
2020-10-04 20:12:56 +0200

    Add Sorting of Result & Compute Whether Game is Completed

--- api/controllers/resultController.js
+++ api/controllers/resultController.js
@@ -1,26 +1,58 @@
-const gametable = require('../constants/game-table');
+const initialGame = require('../constants/game-table');
 const { VALID_CELL_VALUES } = require('../constants/valid-cell-values');
 
+let currentGame;
+
 module.exports.post_result = (req, res) => {
-  res.json(createEmptyGameResponse(filterInvalidValues(req.body)));
+  if (!currentGame) {
+    currentGame = createEmptyGameResponse();
+  }
+
+  currentGame = getUpdatedGameReponse(filterInvalidValues(req.body));
+
+  return res.json(currentGame);
 };
+
+function getUpdatedGameReponse(updatedCells) {
+  const updatedGame = createGametable(currentGame.game, updatedCells);
+  return {
+    game: updatedGame,
+    completed: getIsCompleted(updatedGame),
+  };
+}
 
 function createEmptyGameResponse(updatedCells) {
   return {
-    game: createGametable(updatedCells),
+    game: createGametable(initialGame, updatedCells || []),
     completed: false,
   };
 }
 
-function createGametable(updatedCells) {
-  return [
-    ...gametable.filter(
-      (cell) => !updatedCells.some((updatedCell) => updatedCell.id === cell.id)
-    ),
-    ...updatedCells,
-  ];
+function createGametable(game, updatedCells) {
+  return sortBy(
+    [
+      ...game.filter(
+        (cell) =>
+          !updatedCells.some((updatedCell) => updatedCell.id === cell.id)
+      ),
+      ...updatedCells,
+    ],
+    'id'
+  );
 }
 
 function filterInvalidValues(updatedCells) {
   return updatedCells.filter((cell) => !VALID_CELL_VALUES.includes(cell));
+}
+
+function getIsCompleted(game) {
+  return game.every((cell) => cell.value !== null);
+}
+
+function sortBy(arr, key, direction = 'asc') {
+  if (direction === 'desc') {
+    return arr.sort((a, b) => (a[key] < b[key] ? 0 : -1));
+  }
+
+  return arr.sort((a, b) => (a[key] > b[key] ? 0 : -1));
 }
--- api/controllers/resultController.test.js
+++ api/controllers/resultController.test.js
@@ -1,18 +1,26 @@
 const resultController = require('./resultController');
 const gameplan = require('../constants/game-table');
 const {
+  create_mockRequest_post_result_all_completed,
   create_mockRequest_post_result,
   mockResponse_post_result,
 } = require('../mocks/result.mocks');
 
 describe('ResultController', () => {
+  const getGame = (gameplan, mockRequest) => [
+    ...gameplan.filter(
+      (cell) => !mockRequest.some((mockCell) => mockCell.id === cell.id)
+    ),
+    ...mockRequest,
+  ];
+
   describe('postResult', () => {
     beforeEach(() => {
       spyOn(mockResponse_post_result, 'json').and.callThrough();
     });
 
     it('should return a json response with the updated game', () => {
-      const mockRequest = create_mockRequest_post_result('ttt-2', 'x');
+      const mockRequest = create_mockRequest_post_result('ttt_2', 'x');
 
       resultController.post_result(
         { body: mockRequest },
@@ -20,14 +28,35 @@ describe('ResultController', () => {
       );
 
       const expected = {
-        game: [
-          ...gameplan.filter((cell) => cell.id !== mockRequest[0].id),
-          ...mockRequest,
-        ],
+        game: getGame(gameplan, mockRequest).sort((a, b) =>
+          a.id > b.id ? 1 : -1
+        ),
         completed: false,
       };
 
       expect(mockResponse_post_result.json).toHaveBeenCalledWith(expected);
     });
+  });
+
+  it('should set completed to false if not all cells are filled', () => {
+    const mockRequest = create_mockRequest_post_result('ttt_2', 'x');
+
+    const result = resultController.post_result(
+      { body: mockRequest },
+      mockResponse_post_result
+    );
+
+    expect(result.completed).toBeFalsy();
+  });
+
+  it('should set completed to true if all cells are filled', () => {
+    const mockRequest = create_mockRequest_post_result_all_completed();
+
+    const result = resultController.post_result(
+      { body: mockRequest },
+      mockResponse_post_result
+    );
+
+    expect(result.completed).toBeTruthy();
   });
 });
