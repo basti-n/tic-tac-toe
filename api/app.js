6b2e84cab8fee35dd942e09c4eda81848b60a55b
Sebastian Neumair <sebastianneumair@Sebastians-MBP.fritz.box>
2020-10-08 07:23:55 +0200

    Fix PORT in app.js

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ const resultRoutes = require('./routes/result');
 const { serve, setup } = require('./swagger/init');
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
